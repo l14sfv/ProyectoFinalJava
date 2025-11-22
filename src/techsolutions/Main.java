0259fa47e469fed6ab665100044a3513e67f0cf7
l14sfv <113190914+l14sfv@users.noreply.github.com>
2025-11-21 20:23:59 -0500

    Update Main.java

--- src/techsolutions/Main.java
+++ src/techsolutions/Main.java
@@ -2,8 +2,6 @@ package techsolutions;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
